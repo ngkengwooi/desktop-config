5547c576caf04395b8c738c8e786b1b835458a39
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-01-16 22:58:06 +0000

    Update debian-postinst.sh

--- debian-postinst.sh
+++ debian-postinst.sh
@@ -12,14 +12,29 @@ if [ "$EUID" == 0 ]; then
   echo "deb https://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
   echo "deb https://deb.debian.org/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
   
-  echo "Update packages."
+  ######################
+  # Add Fasttrack repo #
+  ######################
+  apt-get -qq update
+  apt-get -yy install fasttrack-archive-keyring
+  apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib"
+  apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib"
   
+  ############################
+  # Update existing packages #
+  ############################
   apt-get -qq update
   apt-get -yy dist-upgrade
   
-  echo "Install packages."
-  
-  apt-get -yy install $(cat ./packages.txt)
+  ###############################
+  # Install additional packages #
+  ###############################
+  apt-get -yy install lame ffmpeg audacity puddletag soundconverter sound-juicer
+  apt-get -yy install pitivi mkvtoolnix-gui handbrake gnome-subtitles
+  apt-get -yy install gnome-games gnome-mastermind frogatto 0ad unknown horizons supertux supertuxkart warzone2100
+  apt-get -yy install lyx texlive
+  apt-get -yy install evolution evolution-ews
+  apt-get -yy install virtualbox virtualbox-guest-additions-iso
   
 else
   echo "Aborted, user is not root."
