1188cf6c55725d7314c4cfa5a21c4aa941d5a5ae
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-01-16 22:50:59 +0000

    Update debian-postinst.sh

--- debian-postinst.sh
+++ debian-postinst.sh
@@ -3,16 +3,25 @@
 echo "[Setup] Started."
 
 if [ "$EUID" == 0 ]; then
-  echo "[Setup] User is root."
+  echo "User is root."
   
-  echo "[Setup] Update repositories."
+  echo "Update repositories."
   
   echo "deb https://deb.debian.org/debian/ bullseye main non-free contrib" > /etc/apt/sources.list
   echo "deb https://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
   echo "deb https://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
   echo "deb https://deb.debian.org/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
   
+  echo "Update packages."
+  
+  apt-get -qq update
+  apt-get -yy dist-upgrade
+  
+  echo "Install packages."
+  
+  apt-get -yy install $(cat ./packages.txt)
+  
 else
-  echo "[Setup] Aborted, user is not root."
+  echo "Aborted, user is not root."
   exit 1
 fi
