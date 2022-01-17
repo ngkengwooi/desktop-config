0d19736ce4d839126085f6e1a77b898b12fcde7c
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-01-17 22:21:34 +0000

    Update debian-postinst.sh

--- debian-postinst.sh
+++ debian-postinst.sh
@@ -1,56 +1,47 @@
 #!/bin/bash -e
 
+########################
+# Check root privilege #
+########################
 if [ $EUID -eq 0 ]; then
   
-  # By default, the bullseye, bullseye-updates and bullseye-security repos are enabled.
-  # Just need to change the HTTP protocol to HTTPS and remove comments.
-  sed -i "s/http\:\/\//https\:\/\//" /etc/apt/sources.list # Use HTTPS
-  sed -i "/^[[:space:]]*$/d" /etc/apt/sources.list # Remove blank lines
-  set -i "/^\s*#.*/d" /etc/apt/sources.list # Remove comments
-  
-  # Add the backports repo.
-  apt-add-repository "deb https://deb.debian.org/debian/ bullseye-backports main contrib non-free"
-  
-  # Add the fasttrack repo, needed for VirtualBox.
+  ################
+  # Set up repos #
+  ################
+  echo "deb https://deb.debian.org/debian/ bullseye main non-free contrib" > /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
   apt-get -qq update
   apt-get -yy install fasttrack-archive-keyring
   apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib"
   apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib"
   
-  # Upgrade existing packages.
-  # Update repos first since new repos have been added.
+  #############################
+  # Upgrade existing packages #
+  #############################
   apt-get -qq update
   apt-get -yy dist-upgrade
   
-  # Install additional packages.
-  apt-get -yy install $(cat debian-install.txt)
- 
-  # Install flatpaks.
-  apt-get -yy install \
-    flatpak \
-    gnome-software-plugin-flatpak
-  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  flatpak -y install flathub \
-    com.microsoft.Teams \
-    org.zotero.Zotero \
-    us.zoom.Zoom
- 
-  # Make QT apps use GTK theme.
-  apt-get -yy install \
-    adwaita-qt \
-    qt5-gtk-platformtheme \
-    qt5-style-plugins
+  ################################
+  # Enable GTK theme for QT apps #
+  ################################
   echo "QT_QPA_PLATFORMTHEME='gnome'" > /etc/environment
   
-  # Configure the GRUB bootloader.
-  set -i "s/GRUB_TIMEOUT=\d+/GRUB_TIMEOUT=0/" /etc/default/grub
-  sed -i "s/quiet/quiet splash/" /etc/default/grub
+  #############################
+  # Configure GRUB bootloader #
+  #############################
+  sed -i 's/^\s*GRUB_TIMEOUT=\d+\s*$/GRUB_TIMEOUT=0/' /etc/default/grub
+  sed -i 's/^\s*GRUB_CMDLINE_LINUX_DEFAULT="[^"]+"\s*$/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/' /etc/default/grub
   update-grub2
   
-  # Upgrade Python installer and other apps.
-  python3 -m pip install --upgrade pip youtube-dl
+  ###############################
+  # Hand over config to ansible #
+  ###############################
+  apt-get -yy install ansible git
+  ansible-pull -U https://github.com/ngkengwooi/desktop-config debian-workstation.yml
   
 else
-  echo "Aborted, user is not root."
+  echo "Please execute this script as root."
   exit 1
 fi
