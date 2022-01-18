36f1df2bf6e69946b0be91ef8fa9acde61d5e91a
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-01-18 09:46:43 +0000

    Update debian-postinst.sh

--- debian-postinst.sh
+++ debian-postinst.sh
@@ -31,8 +31,8 @@ if [ $EUID -eq 0 ]; then
   #############################
   # Configure GRUB bootloader #
   #############################
-  sed -i 's/GRUB_TIMEOUT=[0-9]+/GRUB_TIMEOUT=0/' /etc/default/grub
-  sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]+"/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/' /etc/default/grub
+  sed -i 's/GRUB_TIMEOUT\=\d+/GRUB_TIMEOUT\=0/' /etc/default/grub
+  sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT\="[^"]+"/GRUB_CMDLINE_LINUX_DEFAULT\="splash quiet"/' /etc/default/grub
   update-grub2
   
   ###############################
