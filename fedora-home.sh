3c2450dcd74624d7fd0f76aba2e5797840046367
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-04-24 16:05:21 +0100

    Update fedora-home.sh

--- fedora-home.sh
+++ fedora-home.sh
@@ -4,12 +4,6 @@
 # curl -sSL https://github.com/ngkengwooi/desktop-config/fedora-home.sh | sudo sh
 # (Piping to shell because I wrote this script and I trust it)
 
-# Check that user is root:
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 # Install Ansible:
 dnf -y install ansible
 
