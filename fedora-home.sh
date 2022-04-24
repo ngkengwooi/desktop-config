fb920500a203a0d661d48dc20cdd47619528337e
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-04-24 15:54:22 +0100

    Create fedora-home.sh

---
+++ fedora-home.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+# Run this script with root privileges, e.g.
+# curl -sSL https://github.com/ngkengwooi/desktop-config/fedora-home.sh | sudo sh
+# (Piping to shell because I wrote this script and I trust it)
+
+# Check that user is root:
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Install Ansible:
+dnf -y install ansible
+
+# Configure system with Ansible:
+ansible-pull -U https://github.com/ngkengwooi/desktop-config fedora-home.yml
