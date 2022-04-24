d7952541a562a748591b4bd933fa8b8c6324be39
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-04-24 16:27:06 +0100

    Update fedora-home.sh

--- fedora-home.sh
+++ fedora-home.sh
@@ -5,7 +5,11 @@
 # (Piping to shell because I wrote this script and I trust it)
 
 # Install Ansible:
-dnf -y install ansible
+dnf -y install ansible flatpak
+
+# Replace the Fedora-curated flathub repo
+# The flathub.org repo will be added back by Ansible Pull
+flatpak remote-delete flathub
 
 # Configure system with Ansible:
 ansible-pull -U https://github.com/ngkengwooi/desktop-config fedora-home.yml
