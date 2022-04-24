33bb9f2b7f0fa92e593fb06669a8fb45d0465445
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-04-24 17:43:47 +0100

    Create debian-home.sh

---
+++ debian-home.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh -e
+
+# Run this script with root privileges, e.g.
+# curl -sSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/main/debian-home.sh | sudo sh
+# (Piping to shell because I wrote this script and I trust it)
+
+# Install Ansible:
+apt-get -y install ansible
+
+# Configure system with Ansible:
+ansible-pull -U https://github.com/ngkengwooi/desktop-config debian-home.yml
