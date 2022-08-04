c133004afebf409cd992ff6d48c077828a16af03
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-08-04 22:39:06 +0100

    Create debian-i386.sh

---
+++ debian-i386.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -e
+
+########################
+# Check root privilege #
+########################
+if [ $EUID -eq 0 ]; then
+  
+  ################
+  # Set up repos #
+  ################
+  echo "deb https://deb.debian.org/debian/ bullseye main non-free contrib" > /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
+  apt-get -qq update
+  #apt-get -yy install fasttrack-archive-keyring
+  #apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib"
+  #apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib"
+  
+  #############################
+  # Upgrade existing packages #
+  #############################
+  #apt-get -qq update
+  apt-get -yy dist-upgrade
+  
+  ################################
+  # Enable GTK theme for QT apps #
+  ################################
+  # echo "QT_STYLE_OVERRIDE=Adwaita" > /etc/environment
+  
+  #############################
+  # Configure GRUB bootloader #
+  #############################
+  sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+  sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/' /etc/default/grub
+  update-grub
+  
+  ###############################
+  # Hand over config to ansible #
+  ###############################
+  apt-get -yy install ansible git
+  ansible-pull -U https://github.com/ngkengwooi/desktop-config debian-i386.yml
+  
+else
+  echo "Please execute this script as root."
+  exit 1
+fi
