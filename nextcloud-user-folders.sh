ff019dadbdbec8a67520a7416a35996bd2ad3c8c
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-09-17 21:30:07 +0100

    Update nextcloud-user-folders.sh

--- nextcloud-user-folders.sh
+++ nextcloud-user-folders.sh
@@ -2,7 +2,7 @@
 # Script to symlink user folders to Nextcloud folders,
 # so that files in these folder are automatically synced to Nextcloud.
 
-for FOLDER in Desktop Documents Downloads Music Pictures Public Templates Videos; do
+for FOLDER in Desktop Documents Music Pictures Public Templates Videos; do
 	mkdir -p "$HOME/Nextcloud/$FOLDER"
 	$(rm -r "$HOME/$FOLDER" | true) && ln -s "$HOME/Nextcloud/$FOLDER" "$HOME/$FOLDER"
 	xdg-user-dirs-update --set "$FOLDER" "$HOME/Nextcloud/$FOLDER"
