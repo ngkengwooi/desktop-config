c8085809f218de5b66ef44ec89413ac350d9eb72
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2022-11-05 20:54:53 +0000

    Update debian-i386.sh

--- debian-i386.sh
+++ debian-i386.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+DEBIAN_RELEASE=bullseye
+
 ########################
 # Check root privilege #
 ########################
@@ -8,10 +10,10 @@ if [ $EUID -eq 0 ]; then
   ################
   # Set up repos #
   ################
-  echo "deb https://deb.debian.org/debian/ bullseye main non-free contrib" > /etc/apt/sources.list
-  echo "deb https://deb.debian.org/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb https://deb.debian.org/debian-security/ bullseye-security main contrib non-free" >> /etc/apt/sources.list
-  echo "deb https://deb.debian.org/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $DEBIAN_RELEASE main non-free contrib" > /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $DEBIAN_RELEASE-updates main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian-security/ $DEBIAN_RELEASE-security main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $DEBIAN_RELEASE-backports main contrib non-free" >> /etc/apt/sources.list
   apt-get -qq update
   
   #############################
@@ -29,7 +31,7 @@ if [ $EUID -eq 0 ]; then
   #############################
   sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
   sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/' /etc/default/grub
-  update-grub
+  update-grub2
   
   ###############################
   # Hand over config to ansible #
