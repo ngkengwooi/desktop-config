ee8a86d8127b93afa726d572a057cf461ca05e02
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2023-06-13 21:38:24 +0100

    Update debian-lab-gnome.sh

--- debian-lab-gnome.sh
+++ debian-lab-gnome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-CODENAME=bullseye
+CODENAME=bookworm
 
 ########################
 # Check root privilege #
@@ -10,13 +10,14 @@ if [ $EUID -eq 0 ]; then
   ################
   # Set up repos #
   ################
-  echo "deb https://deb.debian.org/debian/ $CODENAME main contrib non-free" > /etc/apt/sources.list
-  echo "deb https://deb.debian.org/debian/ $CODENAME-updates main contrib non-free" >> /etc/apt/sources.list
-  echo "deb https://deb.debian.org/debian-security/ $CODENAME-security main contrib non-free" >> /etc/apt/sources.list
-  echo "deb https://deb.debian.org/debian/ $CODENAME-backports main contrib non-free" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $CODENAME main contrib non-free non-free-firmware" > /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $CODENAME-updates main contrib non-free non-free-firmware" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian-security/ $CODENAME-security main contrib non-free non-free-firmware" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $CODENAME-backports main contrib non-free non-free-firmware" >> /etc/apt/sources.list
   apt-get -qq update
   
   # Disable fasttrack repos
+  # Not yet available for bookworm
   #apt-get -yy install fasttrack-archive-keyring
   #apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ $CODENAME-fasttrack main contrib"
   #apt-add-repository "deb https://fasttrack.debian.net/debian-fasttrack/ $CODENAME-backports-staging main contrib"
@@ -24,8 +25,8 @@ if [ $EUID -eq 0 ]; then
   #############################
   # Upgrade existing packages #
   #############################
-  apt-get -qq update
-  apt-get -yy dist-upgrade
+  apt-get update
+  apt-get -y dist-upgrade
   
   ################################
   # Enable GTK theme for QT apps #
@@ -35,7 +36,7 @@ if [ $EUID -eq 0 ]; then
   #############################
   # Configure GRUB bootloader #
   #############################
-  sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=3/' /etc/default/grub
+  sed -Ei "s/GRUB_TIMEOUT=[0-9]+/GRUB_TIMEOUT=3/" /etc/default/grub
   sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/' /etc/default/grub
   update-grub
   
@@ -43,7 +44,7 @@ if [ $EUID -eq 0 ]; then
   # Hand over config to ansible #
   ###############################
   apt-get -yy install ansible git
-  ansible-pull -U https://github.com/ngkengwooi/desktop-config debian-lab-gnome.yml
+  ansible-pull -U https://github.com/ngkengwooi/desktop-config debian-lab-gnome-$CODENAME.yml
   
 else
   echo "Please execute this script as root."
