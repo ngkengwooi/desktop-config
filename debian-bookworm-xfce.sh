0b46aaa5aca68417d9741a9a27710c05d0d24939
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2023-08-12 21:33:43 +0100

    Create debian-bookworm-xfce.sh

---
+++ debian-bookworm-xfce.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -e
+
+CODENAME=bookworm
+
+########################
+# Check root privilege #
+########################
+if [ $EUID -eq 0 ]; then
+  
+  ################
+  # Set up repos #
+  ################
+  echo "deb https://deb.debian.org/debian/ $CODENAME main contrib non-free non-free-firmware" > /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $CODENAME-updates main contrib non-free non-free-firmware" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian-security/ $CODENAME-security main contrib non-free non-free-firmware" >> /etc/apt/sources.list
+  echo "deb https://deb.debian.org/debian/ $CODENAME-backports main contrib non-free non-free-firmware" >> /etc/apt/sources.list
+  apt-get -qq update
+  
+  #############################
+  # Upgrade existing packages #
+  #############################
+  apt-get -yy dist-upgrade
+  
+  ####################################
+  # Make QT and GTK themes get along #
+  ####################################
+  echo "QT_QPA_PLATFORMTHEME=gtk2" > /etc/environment
+  
+  #############################
+  # Configure GRUB bootloader #
+  #############################
+  sed -Ei "s/GRUB_TIMEOUT=[0-9]+/GRUB_TIMEOUT=3/" /etc/default/grub
+  sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="splash quiet"/' /etc/default/grub
+  update-grub2
+  
+  #################################
+  # Make home directories private #
+  # Since bookworm default=0700   #
+  #################################
+  chmod 0700 /home/*/
+  sed -Ei "s/DIR_MODE=[0-9]+/DIR_MODE=0700/" /etc/adduser.conf
+
+  ####################################
+  # Install XFCE desktop environment #
+  ####################################
+  apt-get -y install \
+    fonts-roboto \
+    gnome-system-tools \ # Needed for GUI user management
+    lightdm \
+    lightdm-gtk-greeter-settings \
+    plymouth \
+    task-xfce-desktop \
+    xfce4-goodies \
+    yaru-theme-gtk \
+    yaru-theme-icon
+
+  ##################################
+  # Show usernames on login screen #
+  ##################################
+  sed -Ei "s/#greeter-hide-users=false/greeter-hide-users=false/" /etc/lightdm/lightdm.conf
+  
+  ###############################
+  # Hand over config to ansible #
+  ###############################
+  apt-get -yy install ansible git
+  ansible-pull -U https://github.com/ngkengwooi/desktop-config debian-lab-xfce-$CODENAME.yml
+  
+else
+  echo "Please execute this script as root."
+  exit 1
+fi
