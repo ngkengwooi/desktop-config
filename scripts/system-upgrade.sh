b3fd038ff87101ef31eabd8bf0c7dceec696a12b
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2024-01-01 20:03:30 +0000

    Update system-upgrade.sh

--- scripts/system-upgrade.sh
+++ scripts/system-upgrade.sh
@@ -3,9 +3,9 @@
 ##########################################################
 # This script will install all available updates for any #
 # software installed system-wide via APT or Flatpak.     #
-# Save this script as /usr/local/bin/system-upgrade      #
+# Save this script as /usr/local/sbin/system-upgrade      #
 # and make it executable:                                #
-#   sudo chmod +x /usr/local/bin/system-upgrade          #
+#   sudo chmod +x /usr/local/sbin/system-upgrade          #
 # Invoke as follows:                                     #
 #   sudo system-upgrade                                  #
 ##########################################################
