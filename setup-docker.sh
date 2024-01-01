eac672069d822f82a9a3b3fea5d5bf6b8e6cadfd
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2024-01-01 17:25:22 +0000

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,19 +1,9 @@
 #!/bin/bash -e
-if [ $EUID -eq 0 ]; then
-  echo "{
-  "  default-address-pools" : [
-    {
-      "base" : "172.17.0.0/12",
-      "size" : 20
-    },
-    {
-      "base" : "192.168.0.0/16",
-      "size" : 24
-    }
-  ]
-}" > /etc/docker/daemon.json
-  curl -fsSL https://get.docker.com | bash  
-else
-  echo "Please execute this script as root."
-  exit 1
-fi
+################################################################################
+# Script to set up Docker.
+# Execute as superuser.
+################################################################################
+
+mkdir -p /etc/docker
+curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/docker/daemon.json -o /etc/docker/daemon.json
+curl -fsSL https://get.docker.com | bash
