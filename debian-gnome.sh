86286fcd473fed4e50aaa7ad687166a28d947df2
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2024-01-01 17:49:26 +0000

    Update debian-gnome.sh

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 ################################################################################
-# Configure GNOME desktop
+# Configure GNOME desktop.
+# Execute as superuser.
 ################################################################################
 curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/main/scripts/debian-common.sh | bash
 ansible-pull -U https://github.com/ngkengwooi/desktop-config ansible/debian-gnome.yml
