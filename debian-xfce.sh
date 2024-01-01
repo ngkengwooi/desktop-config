8da5a406a0564b16b71366ef7d37a4398697d055
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2024-01-01 17:20:28 +0000

    Update debian-xfce.sh

--- debian-xfce.sh
+++ debian-xfce.sh
@@ -2,6 +2,6 @@
 ################################################################################
 # Configure GNOME desktop
 ################################################################################
-curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/main/debian-common.sh | bash
+curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/main/scripts/debian-common | bash
 ansible-pull -U https://github.com/ngkengwooi/desktop-config ansible/debian-xfce.yml
 system-upgrade
