c417bd9b70d162fecb9078e4ce0872e6e5d7c244
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2024-01-01 12:17:13 +0000

    Update setup-docker.sh

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -e
 if [ $EUID -eq 0 ]; then
-  curl -fsSL https://get.docker.com | bash
+  echo "{
+  "  default-address-pools" : [
+    {
+      "base" : "172.17.0.0/12",
+      "size" : 20
+    },
+    {
+      "base" : "192.168.0.0/16",
+      "size" : 24
+    }
+  ]
+}" > /etc/docker/daemon.json
+  curl -fsSL https://get.docker.com | bash  
 else
   echo "Please execute this script as root."
   exit 1
