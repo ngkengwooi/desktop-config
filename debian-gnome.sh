18650e5826889af275795d414267b0618ecc7466
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2024-01-02 16:59:15 +0000

    Update debian-gnome.sh

--- debian-gnome.sh
+++ debian-gnome.sh
@@ -4,5 +4,10 @@
 # Execute as superuser.
 ################################################################################
 curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/main/scripts/debian-common.sh | bash
+
+# Harmonise theming for QT apps
+echo "QT_STYLE_OVERRIDE=Adwaita" > /etc/environment
+
 ansible-pull -U https://github.com/ngkengwooi/desktop-config ansible/debian-gnome.yml
+
 system-upgrade
