ff4dcf9b8cc4fb4b5b4a73dce9331392f4693785
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2025-08-03 11:01:44 +0100

    Create trixie-gnome.sh

---
+++ trixie-gnome.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+################################################################################
+# Configure GNOME desktop.
+# Execute as superuser.
+################################################################################
+
+# Apply common configuration
+curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/main/scripts/trixie-common.sh | bash
+
+# Harmonise theming for QT apps
+echo "QT_STYLE_OVERRIDE=Adwaita" > /etc/environment
+
+# Use Ansible to manage apps
+ansible-pull -U https://github.com/ngkengwooi/desktop-config ansible/trixie-gnome.yml
+
+# Final update and cleanup
+system-upgrade
