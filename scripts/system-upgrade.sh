6cd7b1935b4f0f513be22b092d22a616d2d1af77
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2025-11-23 12:09:14 +0000

    Rename system-upgrade script to system-update

--- scripts/system-upgrade.sh
+++ scripts/system-upgrade.sh
@@ -3,11 +3,11 @@
 ##########################################################
 # This script will install all available updates for any #
 # software installed system-wide via APT or Flatpak.     #
-# Save this script as /usr/local/sbin/system-upgrade      #
+# Save this script as /usr/local/sbin/system-update      #
 # and make it executable:                                #
-#   sudo chmod +x /usr/local/sbin/system-upgrade          #
+#   sudo chmod +x /usr/local/sbin/system-update          #
 # Invoke as follows:                                     #
-#   sudo system-upgrade                                  #
+#   sudo system-update                                  #
 ##########################################################
 
 apt-get update
