dd19b96c3849cdb369f6957fff864a10386bf4b5
Keng Wooi Ng <39825599+ngkengwooi@users.noreply.github.com>
2025-11-23 12:33:20 +0000

    Delete docker.sh

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-################################################################################
-# Script to set up Docker.
-# Execute as superuser.
-################################################################################
-
-mkdir -p /etc/docker
-curl -fsSL https://raw.githubusercontent.com/ngkengwooi/desktop-config/docker/daemon.json -o /etc/docker/daemon.json
-curl -fsSL https://get.docker.com | bash
